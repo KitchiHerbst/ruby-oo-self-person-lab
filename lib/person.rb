10c5d4bbb8331ff4f54ec98c8fe026ac2e1685e3
Triston <tristonherbst97@gmail.com>
2021-02-24 14:27:29 -0600

    <Triston> changes

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
 # your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name 
+    attr_accessor :bank_account, :happiness, :hygiene
+    
+    def initialize(name, bank_account=25, happiness=8, hygiene=8)
+        @name=name
+        @bank_account=bank_account
+        @happiness=happiness.range
+        @hygiene=hygiene
+       # binding.pry
+    end
+
+    def range
+        range=(0..10)
+    end
+
+end
+
